6a2adc4824a7b9c159d30a045f5534cc37eea748
饭团 <2133857993@qq.com>
2026-03-20 22:01:18 +0800

    Replace console input/output with Qt application setup

--- 大作业/main.cpp
+++ 大作业/main.cpp
@@ -1,13 +1,15 @@
-#include<iostream>
-#include<cmath>
-using namespace std;
-int main ()
+#include <QApplication>
+#include "GameWindow.h"
+
+int main(int argc, char *argv[])
 {
-	int a, b;
-	cin >> a >> b;
-	cout << b << a;
-	int c;
-	cin >> c;
+    QApplication app(argc, argv);
 
+    // 创建主窗口
+    GameWindow window;
+    window.setWindowTitle("森林冰火人");
+    window.resize(800, 600); // 你可以根据需要调整窗口大小
+    window.show();
 
+    return app.exec();
 }
